f97c4d7a6941647edfea463649e1939778ae8251
Skyrolight <skyrolight@gmail.com>
2023-03-20 11:37:07 +0100

    TD7

--- apiUser/routes/users.js
+++ apiUser/routes/users.js
@@ -2,19 +2,21 @@ var express = require('express');
 var router = express.Router();
 const knex = require('knex')
 
-
 //connectiondb
 let db = knex({
     client: 'mysql',
     connection: {
         host: process.env.MARIADB_HOST,
-        port: 33060,
+        port: 3308,
         user: process.env.MARIADB_USER,
         password: process.env.MARIADB_PASSWORD,
         database: process.env.MARIADB_DATABASE
     }
 });
 
-
-
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.json("salut !");
+});
+  
 module.exports = router;
