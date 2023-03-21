d601c09fae73324b7b639b5c5bcfba9839c71918
Alban-Ktz <alban.ktz@gmail.com>
2023-03-21 17:52:25 +0100

    all get done (with gateway)

--- gateway/routes/orders.js
+++ gateway/routes/orders.js
@@ -2,11 +2,37 @@ var express = require('express');
 var router = express.Router();
 const axios = require('axios');
 
+/* GATEWAY */
+
 /* GET orders page. */
 router.route('/')
     .get(async (req, res, next) => {
         try {
             const response = await axios.get('http://api:3000/orders');
+            res.json(response.data);
+        } catch (error) {
+            console.error(error);
+            next(error);
+        }
+}); 
+
+/* GET a order by his ID. */
+router.route('/:id')
+    .get(async (req, res, next) => {
+        try {
+            const response = await axios.get('http://api:3000/orders/' + req.params.id);
+            res.json(response.data);
+        } catch (error) {
+            console.error(error);
+            next(error);
+        }
+}); 
+
+/* GET all the items of an order */
+router.route('/:id/items')
+    .get(async (req, res, next) => {
+        try {
+            const response = await axios.get('http://api:3000/orders/' + req.params.id + '/items');
             res.json(response.data);
         } catch (error) {
             console.error(error);
