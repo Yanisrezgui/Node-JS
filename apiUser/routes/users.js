21c0254b760543e93d16f68c4871d4b352efdb68
Skyrolight <skyrolight@gmail.com>
2023-03-21 16:18:47 +0100

    TD7

--- apiUser/routes/users.js
+++ apiUser/routes/users.js
@@ -5,6 +5,9 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const randtoken = require('rand-token');
 
+// Créer un access-token JWT
+const secret = process.env.accessToken;
+
 //connectiondb
 let db = knex({
     client: 'mysql',
@@ -22,47 +25,72 @@ router.route('/signup')
     let username = req.body.username;
     let email = req.body.email;
     let password = req.body.password;
-  
-    // Vérifier si l'utilisateur existe déjà
-    const user = await db('client').where('mail_client', email ).first();
-    if (user) {
-      return res.status(409).json({ message: 'Cet utilisateur existe déjà' });
-    } else {
-        const refreshToken = randtoken.generate(16);
-
-        // Insérer l'utilisateur dans la base de données
-        await db('client').insert({
-            nom_client: username,
-            mail_client: email,
-            passwd: bcrypt.hashSync(password, 10),
-            refresh_token: refreshToken
-        });
-      
-        res.status(201).json({refreshToken});
-    }
+    
+    const refreshToken = randtoken.generate(16);
+    // Insérer l'utilisateur dans la base de données
+    await db('client').insert({
+        nom_client: username,
+        mail_client: email,
+        passwd: bcrypt.hashSync(password, 10),
+        refresh_token: refreshToken
+    });
+    
+    res.status(201).json({refreshToken});
 });
 
 router.route('/signin')
     .post(async (req, res) => {
-        // Créer un access-token JWT
-        const secret = process.env.accessToken;
+        if (!req.headers.authorization || req.headers.authorization.indexOf('Basic ') === -1) {
+            return res.status(401).json({ type: 'error', error: 401, message: 'no authorization header present' });
+        } else {
+            // Récupération des credentials dans le header Authorization
+            const credentials = req.headers.authorization.split(' ')[1];
+            const decodedCredentials = Buffer.from(credentials, 'base64').toString('ascii');
+            const [username, password] = decodedCredentials.split(':');
     
-        const payload = {
-            user: username,
-            role: 'user'
+            // Vérification des credentials avec la base de données
+            const user = await db('client').where({mail_client: username.trim()}).first();
+
+            const userPassword = bcrypt.compareSync(password, user.passwd);
+            if (!userPassword) {
+                return res.status(401).json({ type: 'error', error: 401, message: 'invalid credentials' });
+            } else {
+            
+                const payload = {
+                    user: username,
+                    role: 'user'
+                }
+            
+                const options = {
+                    expiresIn: '1h'
+                };
+            
+                const accessToken = jwt.sign(payload, secret, options);
+              
+                // Renvoyer l'access-token
+                res.status(201).json({ 
+                    "access-token": accessToken,
+                    "refresh-token": user.refresh_token
+                });
+            }
         }
-    
-        const options = {
-            expiresIn: '1h'
-        };
-    
-        const accessToken = jwt.sign(payload, secret, options);
-      
-        // Renvoyer l'access-token
-        res.status(201).json({ 
-            "access-token": accessToken,
-            "refresh-token": refreshToken
-        });
-    })
+})
+
+router.route('/validate').get((req, res) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({ message: 'Authorization header missing or invalid' });
+    }
+
+    const token = authHeader.split(' ')[1];
+    try {
+        const decoded = jwt.verify(token, secret);
+        console.log(decoded)
+        return res.status(200).json(decoded);
+    } catch (error) {
+        console.log(error)
+        return res.status(401).json({ message: 'Invalid or expired token' });
+    }
+});
   
 module.exports = router;
